ce4a0b20b3e5da08cfe941787af4019b1bdb4150
Vladimir Khlystov <vkhlystov@quintegro.com>
2026-03-16 13:35:09 +0500

    fix vit config with base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/it-kama/",
+  base: "/it-kama-side-apps/",
   plugins: [react()]
 });
+
